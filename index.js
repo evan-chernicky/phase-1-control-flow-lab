acfb82654c57ea0c1c780169d3f723bb0b872b54
Evan Chernicky <echernicky@gmail.com>
2022-01-31 22:11:13 -0500

    commit tree 0c48bbdb604b4fb50e555c3f17b68b8e5666035e

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(checkFeet){
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+  if (checkFeet <= 400) {
+    return "This one is on me!"
+  } 
+  else if (checkFeet > 2000 && checkFeet <= 2500){
+    return "I will gladly take your thirty bucks."
+  } 
+  else if (checkFeet > 2500){
+    return "No can do."
+  }
+};
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+scuberGreetingForFeet(2333);
+
+function ternaryCheckCity(city){
+
+  let response;
+
+  city === "NYC" ?  response = "Ok, sounds good." : response = "No go.";
+
+  return response;
+
 }
+ternaryCheckCity("NYC")
+
+function switchOnCharmFromTip(tip){
+  
+  switch (tip) {
+    case 'generous':
+      return "Thank you so much.";
+      break;
+    case 'not as generous':
+      return "Thank you.";
+      break;
+    default:
+      return "Bye."
+      break;
+  }
+    
+  }
+
+
+
+switchOnCharmFromTip()
